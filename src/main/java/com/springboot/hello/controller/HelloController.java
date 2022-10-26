65534e82b51b1cab52cee36d96bd7e176835dc67
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-26 21:13:37 +0900

    HelloWorld

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class HelloController {
-
     @RequestMapping(value = "/hello", method= RequestMethod.GET)
     public String hello(){
         return "Hollo World";
