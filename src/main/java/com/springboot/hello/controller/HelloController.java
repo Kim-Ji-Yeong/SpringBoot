0b17743786d46755d4b31620d36f68b8c26fc6a9
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-26 15:51:32 +0900

    HelloWorld

---
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.springboot.hello.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method= RequestMethod.GET)
+    public String hello(){
+        return "Hollo World";
+    }
+    @GetMapping(value = "/name")
+    public String getName(){
+        return "JiYeong";
+    }
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable(@PathVariable("variable") String var){
+        return var;
+    }
+}
