d755677ca4b916d1b908111f271e72edad86a4a9
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-27 15:01:38 +0900

    Controller

--- src/main/java/com/springboot/hello/controller/GetController.java
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,10 +1,13 @@
 package com.springboot.hello.controller;
 
+import com.springboot.hello.domain.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
-public class HelloController {
+public class GetController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
@@ -24,6 +27,19 @@ public class HelloController {
     @GetMapping(value = "/request1")
     public String getVariable (@RequestParam String name, @RequestParam String email, @RequestParam String organization){
         return String.format("%s %s %s", name, email, organization);
+    }
+
+    @GetMapping(value = "/request2")
+    public String getVariable2(@RequestParam Map<String, String> param) {
+        param.entrySet().forEach((map)->{
+            System.out.printf("key:%s value:%s\n",map.getKey(),map.getValue());
+        });
+        return "request2가 호출 완료 되었습니다";
+    }
+
+    @GetMapping(value = "/request3")
+    public String getRequestParam(MemberDto memberDto){
+        return memberDto.toString();
     }
 }
 
