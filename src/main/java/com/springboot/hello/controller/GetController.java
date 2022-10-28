634096f451b06ebae2996fb7b41f7b6e5d0ffa2e
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-28 11:36:21 +0900

    log.info

--- src/main/java/com/springboot/hello/controller/GetController.java
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,28 +1,36 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
+        log.info("hello로 요청이 들어왔습니다");
         return "Hollo World";
     }
 
     @GetMapping(value = "/name")
     public String getName() {
+        log.info("getName으로 요청이 들어왔습니다.");
         return "JiYeong";
     }
 
-    @GetMapping(value = "/variable2/{variable}")
-    public String getVariable(@PathVariable("variable") String var) {
-        return var;
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        log.info("getVariable1으로 요청이 들어왔습니다. variable:{}", variable);
+        return variable;
     }
+
+
 
     @GetMapping(value = "/request1")
     public String getVariable (@RequestParam String name, @RequestParam String email, @RequestParam String organization){
