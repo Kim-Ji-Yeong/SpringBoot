66aff2caa5d812edf73f7f1c1dd1b9c9ea430b36
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-28 15:32:16 +0900

    수정중

--- src/main/java/com/springboot/hello/controller/UserController.java
+++ src/main/java/com/springboot/hello/controller/UserController.java
@@ -3,6 +3,7 @@ package com.springboot.hello.controller;
 import com.springboot.hello.dao.UserDao;
 import com.springboot.hello.domain.User;
 import com.springboot.hello.domain.dto.UserRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,24 +12,27 @@ import java.sql.SQLException;
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
-    private UserDao userDao = null;
+    private final UserDao userDao;
 
-    public UserController(UserDao userDao) throws SQLException {
+    public UserController(UserDao userDao) {
         this.userDao = userDao;
     }
 
-    @GetMapping("/")
-    public String Hello() {
-        return "Hello World";
+    @PostMapping("/user")
+    public String add(@RequestBody User user) {
+        userDao.add(user);
+        return String.format("%s번에 정보가 등록되었습니다.", user.getId());
     }
 
-    @GetMapping("/user")
-    public User addAndGet() throws SQLException {
-        userDao.add(new User("1", "Seoyi", "6415"));
-        return userDao.findByID("2");
+    @DeleteMapping(value = "/user/{id}")
+    public String deleteById(@PathVariable String id) {
+        userDao.deleteById(id);
+        return String.format("%s번의 정보가 삭제되었습니다.", id);
     }
-    @DeleteMapping("/user")
-    public ResponseEntity<Integer> deleteAll(){
-        return ResponseEntity.ok().body(userDao.deleteAll());
+
+    @DeleteMapping(value = "/user/all")
+    public String deleteAll() {
+        userDao.deleteAll();
+        return "모든 정보가 삭제되었습니다.";
     }
 }
--- src/main/java/com/springboot/hello/dao/UserDao.java
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -21,25 +21,40 @@ public class UserDao {
         this.dataSource = dataSource;
         this.jdbcTemplate = jdbcTemplate;
     }
-    RowMapper<User> rowMapper = new RowMapper<User>() {
+
+    RowMapper rowMapper = new RowMapper() {
         @Override
-        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-            User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
+        public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"), rs.getString("name"),
+                    rs.getString("password"));
             return user;
         }
     };
 
     public void add(User user) {
-        this.jdbcTemplate.update("insert into users(id, name, password) values (?,?,?)",
+        this.jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES (?, ?, ?)",
                 user.getId(), user.getName(), user.getPassword());
     }
 
-    public int deleteAll() {
-        return this.jdbcTemplate.update("delete from users");
+    public void findById(String id) {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        this.jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
-    public User findByID(String id) throws SQLException {
-        String sql = "select *from users where id = ?";
-        return jdbcTemplate.queryForObject(sql,rowMapper,id);
+    public List<User> getAll() {
+        String sql = "SELECT * FROM users order by id";
+        return this.jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public void deleteById(String id) {
+        this.jdbcTemplate.update("delete from users where id = ?", id);
+    }
+
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount() {
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 }
