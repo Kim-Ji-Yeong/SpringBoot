8955135805067efd437679c173a8595352e22b27
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-31 14:45:05 +0900

    소수점 수정

--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -33,7 +33,7 @@ class HospitalParserTest {
         assertEquals(1, hospital.getHealthcareProviderCount());
         assertEquals(0, hospital.getPatientRoomCount());
         assertEquals(0, hospital.getTotalNumberOfBeds());
-        assertEquals(52.29, hospital.getTotalAreaSize());
+        assertEquals(52.29f, hospital.getTotalAreaSize());
 
     }
 
